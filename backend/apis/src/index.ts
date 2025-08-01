51a101ce08829af38d1061464d7e3c969b196d90
Ankush Agrawal <agrawalankush65@gmail.com>
2025-08-01 12:52:47 +0530

    api getway added

---
+++ backend/apis/src/index.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify'
+
+// implement fastify
+const PORT = process.env.PORT || 4000;
+const app = fastify({ logger: false, trustProxy: true });
+
+// implement fastify register plugins
+
+// Register routes with '/api' prefix
+app.get('/', async (request, reply) => {
+  reply.send('Welcome to apis');
+});
+
+(async () => {
+  try {
+
+    app.listen({ port: Number(PORT) || 4000 }, (err, address) => {
+      if (err) process.exit(1);
+    });
+    console.log(`Server running at http://localhost:${PORT}`);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
